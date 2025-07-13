2d2f4af3e48179cf907c981ed071098cb87aac52
Garima Bhayana <garimaabhayanaa@gmail.com>
2025-07-14 03:05:54 +0530

    Updated app.py

--- backend/app.py
+++ backend/app.py
@@ -333,5 +333,4 @@ def internal_error(error):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # For Render compatibility
-    app.run(host="0.0.0.0", port=port)
+    app.run(port=5000, debug=True)
